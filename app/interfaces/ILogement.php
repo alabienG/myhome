e1c031b3b9ee00f155a489f7ba74d3d7b0bf7965
Gouchere Georges <31745977+alabienG@users.noreply.github.com>
2022-06-10 11:38:42 +0200

    Initial commit

---
+++ app/interfaces/ILogement.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\interfaces;
+
+
+use Illuminate\Http\Request;
+
+interface ILogement
+{
+
+    public function index();
+
+    public function store(Request $request);
+
+    public function update(Request $request, $id);
+
+    public function show($id);
+
+    public function desable($id);
+
+    public function enable($id);
+
+    public function destroy($id);
+
+    public function listByProprio();
+}
